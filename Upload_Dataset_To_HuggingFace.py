c2dcc9d494109821f2f0b33ac1b7f75c42c0af0a
Adrián García García <adriacarlos2004@gmail.com>
2026-03-24 00:06:43 +0100

    Update path

--- Upload_Dataset_To_HuggingFace.py
+++ Upload_Dataset_To_HuggingFace.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 from huggingface_hub import login
 
 login() # Nos logeamos en Hugging Face para subir el dataset
-dataset = load_dataset("json", data_files="./3_3_García_García_Adrián_tlou_dataset.jsonl") # Cargamos el dataset de nuestro equipo
+dataset = load_dataset("json", data_files="./tlou_dataset.jsonl") # Cargamos el dataset de nuestro equipo
 
 dataset = dataset["train"].train_test_split(test_size=0.1) # Lo dividmos en train y test para subirlo a Hugging Face
 
